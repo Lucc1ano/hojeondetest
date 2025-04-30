7cf9dc98a4168b355d1a62b2939ddfe93ad1143e
Lucc1ano <lucianomirosmar@outlook.com>
2025-04-30 20:08:43 -0300

    Update UserController.java

--- hoje/src/main/java/com/hojeonde/hoje/controllers/UserController.java
+++ hoje/src/main/java/com/hojeonde/hoje/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.hojeonde.hoje.controllers;
 import com.hojeonde.hoje.models.User;
 import com.hojeonde.hoje.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,27 +11,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public List<User> getAllUsers() {
+    public ResponseEntity<List<User>> listarUsers() {
         return userService.findAll();
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
+    public ResponseEntity<User> buscarUserPorId(@PathVariable Long id) {
         return userService.findById(id);
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
+    public ResponseEntity<User> criarUser(@RequestBody User user) {
         return userService.save(user);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        userService.delete(id);
+    public ResponseEntity<Void> deletarUser(@PathVariable Long id) {
+        return userService.delete(id);
     }
 }
-
