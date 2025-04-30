c61384e4bd250d50aff863892057823c5ab4511b
Lucc1ano <lucianomirosmar@outlook.com>
2025-04-30 20:17:18 -0300

    Update Local.java

--- hoje/src/main/java/com/hojeonde/hoje/models/Local.java
+++ hoje/src/main/java/com/hojeonde/hoje/models/Local.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Local {
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.UUID)
     private UUID id_local;
     private String nome;
     private String endereço;
