8cc77c9ba1ac1859d952f82172814d8ebe62cb1b
Lucc1ano <lucianomirosmar@outlook.com>
2025-04-30 19:52:17 -0300

    Update EventoController.java

--- hoje/src/main/java/com/hojeonde/hoje/controllers/EventoController.java
+++ hoje/src/main/java/com/hojeonde/hoje/controllers/EventoController.java
@@ -3,6 +3,7 @@ package com.hojeonde.hoje.controllers;
 import com.hojeonde.hoje.models.Evento;
 import com.hojeonde.hoje.services.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,27 +11,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/eventos")
 public class EventoController {
+
     @Autowired
     private EventoService eventoService;
 
     @GetMapping
-    public List<Evento> getAllEventos() {
+    public ResponseEntity<List<Evento>> listarEventos() {
         return eventoService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Evento getEventoById(@PathVariable String id) {
+    public ResponseEntity<Evento> buscarEventoPorId(@PathVariable String id) {
         return eventoService.findById(id);
     }
 
     @PostMapping
-    public Evento createEvento(@RequestBody Evento evento) {
+    public ResponseEntity<Evento> criarEvento(@RequestBody Evento evento) {
         return eventoService.save(evento);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteEvento(@PathVariable String id) {
-        eventoService.delete(id);
+    public ResponseEntity<Void> deletarEvento(@PathVariable String id) {
+        return eventoService.delete(id);
     }
-}
 
+    // Outros métodos do controller (atualizar, etc.) seguiriam o mesmo padrão
+}
