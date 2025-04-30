a64188e4fd593ffed20d27d8fec6f2b2f34a5288
Lucc1ano <lucianomirosmar@outlook.com>
2025-04-30 19:59:53 -0300

    Update IngressosController.java

--- hoje/src/main/java/com/hojeonde/hoje/controllers/IngressosController.java
+++ hoje/src/main/java/com/hojeonde/hoje/controllers/IngressosController.java
@@ -3,34 +3,36 @@ package com.hojeonde.hoje.controllers;
 import com.hojeonde.hoje.models.Ingressos;
 import com.hojeonde.hoje.services.IngressosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/ingressos")
 public class IngressosController {
+
     @Autowired
     private IngressosService ingressosService;
 
     @GetMapping
-    public List<Ingressos> getAllIngressos() {
+    public ResponseEntity<List<Ingressos>> listarIngressos() {
         return ingressosService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Ingressos getIngressoById(@PathVariable String id) {
+    public ResponseEntity<Ingressos> buscarIngressoPorId(@PathVariable UUID id) {
         return ingressosService.findById(id);
     }
 
     @PostMapping
-    public Ingressos createIngresso(@RequestBody Ingressos ingresso) {
+    public ResponseEntity<Ingressos> criarIngresso(@RequestBody Ingressos ingresso) {
         return ingressosService.save(ingresso);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteIngresso(@PathVariable String id) {
-        ingressosService.delete(id);
+    public ResponseEntity<Void> deletarIngresso(@PathVariable UUID id) {
+        return ingressosService.delete(id);
     }
 }
-
