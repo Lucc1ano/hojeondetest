137e73eb3f20743832d80a33ccea79634c44eafa
Lucc1ano <lucianomirosmar@outlook.com>
2025-04-30 20:07:21 -0300

    Update OrganizadorController.java

--- hoje/src/main/java/com/hojeonde/hoje/controllers/OrganizadorController.java
+++ hoje/src/main/java/com/hojeonde/hoje/controllers/OrganizadorController.java
@@ -3,34 +3,36 @@ package com.hojeonde.hoje.controllers;
 import com.hojeonde.hoje.models.Organizador;
 import com.hojeonde.hoje.services.OrganizadorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/organizadores")
 public class OrganizadorController {
+
     @Autowired
     private OrganizadorService organizadorService;
 
     @GetMapping
-    public List<Organizador> getAllOrganizadores() {
+    public ResponseEntity<List<Organizador>> listarOrganizadores() {
         return organizadorService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Organizador getOrganizadorById(@PathVariable String id) {
+    public ResponseEntity<Organizador> buscarOrganizadorPorId(@PathVariable UUID id) {
         return organizadorService.findById(id);
     }
 
     @PostMapping
-    public Organizador createOrganizador(@RequestBody Organizador organizador) {
+    public ResponseEntity<Organizador> criarOrganizador(@RequestBody Organizador organizador) {
         return organizadorService.save(organizador);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteOrganizador(@PathVariable String id) {
-        organizadorService.delete(id);
+    public ResponseEntity<Void> deletarOrganizador(@PathVariable UUID id) {
+        return organizadorService.delete(id);
     }
 }
-
